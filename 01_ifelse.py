fceb12d3d107206fc74da974bdd7fcdefd91c078
Mudassir Ali <deadlool45@gmail.com>
2024-09-26 20:10:06 +0500

    if else statement

--- 01_ifelse.py
+++ 01_ifelse.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+
+score = input("What is your score?") 
+
+score = int(score)
+
+if score >= 90:
+    print("Your frade is A")
+elif score >= 50:
+    print("Your grade is B")
+elif score >= 40:
+    print("Your grade is C")
+else:
+    print("Your grade is F")
+
+
